1994dc1ff25c1c892feed20a086758af3d2c20b5
CuentaEjemplo1CASE <angeldavidmc20001@gmail.com>
2022-07-04 12:53:37 -0500

    David Morales Mayor o Menor

--- Prac1.py
+++ Prac1.py
@@ -10,3 +10,20 @@ if(parOimpar % 2 == 0):
     print("Numero par")
 else:
     print("Numero impar")
+
+#menor o mayor de tres numeros 
+a = int(input("ingrese un numero "))
+b = int(input("ingrese un numero "))
+c = int(input("ingrese un numero "))
+if a > b and a>c:
+    print ("el mayor es",a)
+if b > a and b >c:
+    print ("el mayor es",b)
+if c > a and c >b:
+    print ("el mayor es",c)
+if a < b and a<c:
+    print ("el menor es ",a)
+if b < a and b<c:
+    print ("el menor es",b)
+if c < a and c<b:
+    print ("el menor es",c)
