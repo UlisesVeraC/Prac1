71a5dc327a2f9defc77077a8bd87184a2d7fc2ce
MiguelHeMa <miguelhm@micorreo.upp.edu.mx>
2022-07-04 22:06:34 -0500

    actualizado

--- Prac1.py
+++ Prac1.py
@@ -31,4 +31,9 @@ if b < a and b<c:
 if c < a and c<b:
     print ("el menor es",c)
 
+num1= int(input("Ingrese el primer numero"))
+
+num2= int(input("Ingrese el segundo numero"))
+
+print("la suma de los numero es:", num1 + num2)
     #comentario sss
